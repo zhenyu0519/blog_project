ac342d8a75463b423ada58d7b1bd9215febfbf0a
Zhenyu <zhenyu051988@gmail.com>
2017-09-21 02:21:09 -0700

    get the year and month from http request in order to read the proper archives from database

--- blog_app/views.py
+++ blog_app/views.py
@@ -15,6 +15,8 @@ def index(request):
             article_list = paginator.page(1)
 
         archive_list = Article.objects.distinct_date()
+        # for a in archive_list:
+        #     print a
     except Exception as e:
         print e
     return render(request,'index.html',locals())
@@ -38,10 +40,23 @@ def post(request):
     return render(request, 'post.html', locals())
 
 def article(request):
-    article_list = Article.objects.all()
     return render(request, 'article.html', locals())
 
 def about(request):
     return render(request, 'about.html', locals())
 
-
+def archive(request):
+    try:
+        year = request.GET.get('year', None)
+        month = request.GET.get('month', None)
+        article_list = Article.objects.filter(published_date__icontains=year+'-'+month)
+        paginator = Paginator(article_list, 5)
+        try:
+            page = int(request.GET.get('page', 1))
+            article_list = paginator.page(page)
+        except(EmptyPage, InvalidPage, PageNotAnInteger):
+            article_list = paginator.page(1)
+        archive_list = Article.objects.distinct_date()
+    except Exception as e:
+        pass
+    return render(request,'archive.html',locals())
